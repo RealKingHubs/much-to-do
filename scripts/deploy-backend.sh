865cb54063bb2b00fd20eb2e80dc44abae4e35e5
RealKingHubs <odokingsleyu@gmail.com>
2026-05-17 13:10:10 +0100

    Updated Deploy Script

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-set -e
-
-IMAGE_URI=$1
-AWS_REGION="us-east-1"
+IMAGE=$1
 
 INSTANCE_IDS=$(aws ec2 describe-instances \
-  --region $AWS_REGION \
-  --filters "Name=tag:Name,Values=dev-backend" \
-            "Name=instance-state-name,Values=running" \
+  --filters "Name=tag:Name,Values=dev-backend*" \
   --query "Reservations[*].Instances[*].InstanceId" \
   --output text)
 
-for INSTANCE_ID in $INSTANCE_IDS
-do
-  echo "Deploying to $INSTANCE_ID"
+aws ssm send-command \
+  --instance-ids $INSTANCE_IDS \
+  --document-name "AWS-RunShellScript" \
+  --parameters commands="
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 093796422475.dkr.ecr.us-east-1.amazonaws.com
 
-  aws ssm send-command \
-    --region $AWS_REGION \
-    --instance-ids "$INSTANCE_ID" \
-    --document-name "AWS-RunShellScript" \
-    --parameters commands="
-      docker stop backend || true
-      docker rm backend || true
-      aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $IMAGE_URI
-      docker pull $IMAGE_URI
-      docker run -d --name backend -p 8080:8080 $IMAGE_URI
-    "
-done
+    docker pull $IMAGE
+
+    docker stop backend || true
+    docker rm backend || true
+
+    docker run -d \
+      --name backend \
+      -p 8080:8080 \
+      -e PORT=8080 \
+      -e MONGO_URI='YOUR_MONGO_URI' \
+      -e DB_NAME='much_todo_db' \
+      -e JWT_SECRET_KEY='YOUR_SECRET' \
+      $IMAGE
+  " \
+  --region us-east-1
